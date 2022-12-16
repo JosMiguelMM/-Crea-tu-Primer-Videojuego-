46ff2fedb24602b9ed0473b1c285de2610cc1f64
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2022-12-16 16:05:12 -0500

    Editando

--- game.js
+++ game.js
@@ -7,6 +7,30 @@ window.addEventListener('load', startGame);
 
 function startGame() {
     // lugar donde inicia el trazo(se genera un cuadrado)
-    game.fillRect(0, 0, 100, 100);
-    game.clearRect(0, 0, 50, 50); //borra el cuadrado, segun las coordenadas
+    //game.fillStyle = 'purple'; //color del cuadrado
+    //game.fillRect(0, 50, 100, 100);
+    //game.clearRect(0, 0, 50, 50); //borra el cuadrado, segun las coordenadas
+    //game.font = '30px Arial'; //tamaño de la fuente
+    //game.fillText('Hola', 2, 6); //escribe el texto
+
+    let canvasSize;
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    // para ajustar el tamaño del canvas al tamaño de la ventana
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const ElementsSize = canvasSize / 10;
+    console.log(canvasSize, ElementsSize);
+    game.font = ElementsSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], (ElementsSize * i), (ElementsSize));
+    }
+
 }
